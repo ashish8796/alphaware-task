3e302dc11e0eebbf29af912aa6c7305cad3ed15e
ashish8796 <ashishsaini.ak@gmail.com>
2024-08-21 12:20:48 +0530

    changed removed commented code from server

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,8 +18,3 @@ app.use("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// DB_PASSWORD = urvY1hmnrV5Q3oYq
-// DB_USERNAME = ashishsainiak
-// PORT = 5500
-// JWT_SECRET = "Thisisjwtsecret"

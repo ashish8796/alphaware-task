733e635755d1671f0f99a6afaafab93ed25fa650
Ashish Kumar Saini <ashishsaini.ak@gmail.com>
2024-08-21 16:07:59 +0530

    React routing (#7)
    * added pages for basic layout
    
    * Routing pages are added

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,44 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Use createRoot from react-dom/client
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  BrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store"; // Import the Redux store
+import store from "./redux/store";
 
-// Create a root using the new API
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import JobListings from "./pages/JobListings";
+import JobDetails from "./pages/JobDetails";
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "signup", element: <Signup /> },
+      { path: "login", element: <Login /> },
+      { path: "jobs", element: <JobListings /> },
+      { path: "jobs/:jobId", element: <JobDetails /> },
+      { path: "profile", element: <Profile /> },
+      { path: "dashboard", element: <Dashboard /> },
+    ],
+  },
+]);
+
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container!); // CreateRoot requires a non-null container
+const root = ReactDOM.createRoot(container!);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );

a5bae6b363939e553734934252d73726c2a0692e
ashish8796 <ashishsaini.ak@gmail.com>
2024-08-21 12:01:23 +0530

    created user schema

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,4 +1,15 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  jobs: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 const userSchema = new Schema({
   name: {
@@ -14,6 +25,48 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  jobs: {
+    type: [Schema.Types.ObjectId],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+userSchema.pre("save", hashPassword);
+
+async function hashPassword(this: IUser, next: Function) {
+  if (this.password && this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  next();
+}
+
+async function verifyToken(this: IUser, token: string) {}
+
+async function generateToken(this: IUser) {
+  try {
+    const payload = { email: this.email, userId: this._id };
+    return await jwt.sign(payload, process.env.JWT_SECRET as string, {
+      expiresIn: "5 mins",
+    });
+  } catch (error) {}
+}
+
+async function verifyPassword(this: IUser, password: string) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    console.log("Error in verifying password: ", error);
+    throw error;
+  }
+}
 
 export default mongoose.model("User", userSchema);
